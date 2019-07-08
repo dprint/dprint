178367076cbbd4ba0f61677e45222426d37d78c8
David Sherret <dsherret@gmail.com>
2019-07-08 18:14:42 -0400

    Start to add createPublicRepo.ts script.

---
+++ scripts/createPublicRepo.ts
@@ -0,0 +1,22 @@
+import * as path from "path";
+import { Project } from "ts-morph";
+
+const project = new Project();
+const fileSystem = project.getFileSystem();
+
+// todo: clear out existing files
+copy("implemented-nodes.md");
+copy("mocha.opts");
+copy("package.json"); // todo: strip this down using a whitelist of properties to keep
+copy("README.md");
+copy("dist/cli.js");
+copy("dist/dprint.js");
+copy("src/configuration/dprint.schema.json", "dprint.schema.json");
+copy("src/tests/specs");
+copy("src/tests/runSpecs.ts");
+copy("src/tests/specParser.ts");
+
+function copy(filePath: string, fileTo?: string) {
+    fileTo = fileTo || filePath;
+    fileSystem.copySync(filePath, path.join("./public/", fileTo));
+}
