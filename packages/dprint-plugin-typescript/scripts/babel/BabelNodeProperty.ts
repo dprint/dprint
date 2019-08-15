14d5aa75c7351da0f453679b4d3ca1770fcae22a
David Sherret <dsherret@gmail.com>
2019-08-15 00:01:06 -0400

    Add a warning when an ignored property is referenced more than once for the implemented nodes report.

--- packages/dprint-plugin-typescript/scripts/babel/BabelNodeProperty.ts
+++ packages/dprint-plugin-typescript/scripts/babel/BabelNodeProperty.ts
@@ -10,20 +10,32 @@ export class BabelNodeProperty {
     }
 
     getIgnoredReasonMessage() {
-        for (const node of this.findReferencesAsNodes()) {
-            // can't wait for conditional property access...
-            const propRef = node.getParentIfKind(SyntaxKind.QualifiedName);
-            const typeQuery = propRef && propRef.getParentIfKind(SyntaxKind.TypeQuery);
-            const typeReference = typeQuery && typeQuery.getParentIfKind(SyntaxKind.TypeReference);
-            if (typeReference && typeReference.getTypeName().getText() === "AnalysisMarkIgnored") {
-                // assume this is done correctly...
-                const typeLiteral = typeReference.getTypeArguments()[1] as LiteralTypeNode;
-                const stringLiteral = typeLiteral.getLiteral() as StringLiteral;
-                return stringLiteral.getLiteralValue();
-            }
+        const nodes = this.findReferencesAsNodes();
+        const message = getMessage();
+
+        if (message != null && nodes.length > 1) {
+            // todo: add the property and parent name to this message
+            console.warn("A node was marked to be ignored, but it is used in more than one place.");
         }
 
-        return undefined;
+        return message;
+
+        function getMessage() {
+            for (const node of nodes) {
+                // can't wait for conditional property access...
+                const propRef = node.getParentIfKind(SyntaxKind.QualifiedName);
+                const typeQuery = propRef && propRef.getParentIfKind(SyntaxKind.TypeQuery);
+                const typeReference = typeQuery && typeQuery.getParentIfKind(SyntaxKind.TypeReference);
+                if (typeReference && typeReference.getTypeName().getText() === "AnalysisMarkIgnored") {
+                    // assume this is done correctly...
+                    const typeLiteral = typeReference.getTypeArguments()[1] as LiteralTypeNode;
+                    const stringLiteral = typeLiteral.getLiteral() as StringLiteral;
+                    return stringLiteral.getLiteralValue();
+                }
+            }
+
+            return undefined;
+        }
     }
 
     isReferenced() {
