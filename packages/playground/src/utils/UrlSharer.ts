b9358cadcb0153173cec27dd8bd5d295f4951f07
David Sherret <dsherret@gmail.com>
2019-09-07 17:56:20 -0400

    Add url saving to playground.

---
+++ packages/playground/src/utils/UrlSharer.ts
@@ -0,0 +1,53 @@
+import { TypeScriptConfiguration } from "dprint-plugin-typescript";
+import { decompressFromEncodedURIComponent, compressToEncodedURIComponent } from "lz-string"
+
+export class UrlSaver {
+    getUrlInfo() {
+        const locationHash = document.location.hash || "";
+
+        return {
+            text: getText(),
+            config: getConfig()
+        };
+
+        function getText() {
+            const matches = /code\/([^/]+)/.exec(locationHash);
+            if (matches == null || matches.length !== 2)
+                return "";
+
+            try {
+                return decompress(matches[1]);
+            } catch (err) {
+                console.error(err);
+                return "";
+            }
+        }
+
+        function getConfig(): TypeScriptConfiguration {
+            const matches = /config\/([^/]+)/.exec(locationHash);
+            if (matches == null || matches.length !== 2)
+                return getDefaultConfig();
+
+            try {
+                return JSON.parse(decompress(matches[1]));
+            } catch (err) {
+                console.error(err);
+                return getDefaultConfig();
+            }
+
+            function getDefaultConfig(): TypeScriptConfiguration {
+                return {
+                    lineWidth: 80
+                };
+            }
+        }
+    }
+
+    updateUrl({ text, config }: { text: string; config: TypeScriptConfiguration}) {
+        document.location.hash = `code/${compressToEncodedURIComponent(text)}/config/${compressToEncodedURIComponent(JSON.stringify(config))}`;
+    }
+}
+
+function decompress(text: string) {
+    return decompressFromEncodedURIComponent(text.trim()) || ""; // will be null on error
+}
