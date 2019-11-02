79e2077543e71b0ea0330a83e465cbb162e76b98
David Sherret <dsherret@gmail.com>
2019-11-02 14:07:03 -0400

    rust-core: Use TCondition in ConditionRef.

--- packages/rust-core/src/get_write_items.rs
+++ packages/rust-core/src/get_write_items.rs
@@ -7,8 +7,9 @@ use super::WriteItem;
 
 /// Gets write items from the print items.
 pub fn get_write_items<TString, TInfo, TCondition>(
-    print_items: Vec<PrintItem<TString, TInfo, TCondition>>, options: PrintOptions
-) -> Vec<WriteItem<TString>> where TString : StringRef, TInfo : InfoRef, TCondition : ConditionRef<TString, TInfo> {
+    print_items: Vec<PrintItem<TString, TInfo, TCondition>>,
+    options: PrintOptions
+) -> Vec<WriteItem<TString>> where TString : StringRef, TInfo : InfoRef, TCondition : ConditionRef<TString, TInfo, TCondition> {
     let printer = Printer::new(print_items, options);
     printer.print()
 }
