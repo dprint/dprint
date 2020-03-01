664a08b60bb1a3ec90e9f3b1052ea9264e3a69c3
David Sherret <dsherret@users.noreply.github.com>
2020-03-01 16:51:31 -0500

    feat: #94 - Add preferHanging configuration for statements

--- packages/rust-core/src/condition_resolvers.rs
+++ packages/rust-core/src/condition_resolvers.rs
@@ -1,14 +1,14 @@
 use super::print_items::*;
 
-pub fn is_start_of_new_line(condition_context: &ConditionResolverContext) -> bool {
-    return condition_context.writer_info.column_number == condition_context.writer_info.line_start_column_number;
+pub fn is_start_of_line(condition_context: &ConditionResolverContext) -> bool {
+    condition_context.writer_info.is_start_of_line()
 }
 
 pub fn is_multiple_lines(condition_context: &mut ConditionResolverContext, start_info: &Info, end_info: &Info) -> Option<bool> {
     let start_info = condition_context.get_resolved_info(start_info)?;
     let end_info = condition_context.get_resolved_info(end_info)?;
 
-    return Some(end_info.line_number > start_info.line_number);
+    Some(end_info.line_number > start_info.line_number)
 }
 
 pub fn is_hanging(condition_context: &mut ConditionResolverContext, start_info: &Info, end_info: &Option<Info>) -> Option<bool> {
