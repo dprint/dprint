8742253d4b18cafe4c3b20dea9caa61798597c52
David Sherret <dsherret@gmail.com>
2020-05-04 12:03:31 -0400

    chore: Increase versions.

--- packages/rust-dprint/src/configuration/get_init_config_file_text.rs
+++ packages/rust-dprint/src/configuration/get_init_config_file_text.rs
@@ -1,6 +1,6 @@
 pub fn get_init_config_file_text() -> &'static str {
     r#"{
-  "projectType": "", // required. Possible options according to your conscience: openSource, commercialSponsored, commercialDidNotSponsor
+  "projectType": "",
   "typescript": {},
   "json": {},
   "includes": ["**/*.{ts,tsx,js,jsx,json}"],
