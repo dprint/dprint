cc417aca1e2cc45b5bff8a6cf82f14e326045dda
David Sherret <dsherret@gmail.com>
2020-05-25 11:20:01 -0400

    chore: Add install scripts - Copy & pasted from Deno
    https://github.com/denoland/deno_install

---
+++ scripts/website-assets/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# Copy, paste, replace from Deno's install script at https://github.com/denoland/deno_install/blob/master/install.sh
+# Copyright 2019 the Deno authors. All rights reserved. MIT license.
+# Keep this script simple and easily auditable.
+
+set -e
+
+case $(uname -s) in
+Darwin) target="x86_64-apple-darwin" ;;
+*) target="x86_64-unknown-linux-gnu" ;;
+esac
+
+if [ $(uname -m) != "x86_64" ]; then
+	echo "Unsupported architecture $(uname -m). Only x64 binaries are available."
+	exit
+fi
+
+if [ $# -eq 0 ]; then
+	dprint_asset_path=$(
+		command curl -sSf https://github.com/dprint/dprint/releases |
+			command grep -o "/dprint/dprint/releases/download/.*/dprint-${target}\\.zip" |
+			command head -n 1
+	)
+	if [ ! "$dprint_asset_path" ]; then exit 1; fi
+	dprint_uri="https://github.com${dprint_asset_path}"
+else
+	dprint_uri="https://github.com/dprint/dprint/releases/download/${1}/dprint-${target}.zip"
+fi
+
+dprint_install="${DPRINT_INSTALL:-$HOME/.dprint}"
+bin_dir="$dprint_install/bin"
+exe="$bin_dir/dprint"
+
+if [ ! -d "$bin_dir" ]; then
+	mkdir -p "$bin_dir"
+fi
+
+curl --fail --location --progress-bar --output "$exe.zip" "$dprint_uri"
+cd "$bin_dir"
+unzip -o "$exe.zip"
+chmod +x "$exe"
+rm "$exe.zip"
+
+echo "Dprint was installed successfully to $exe"
+if command -v dprint >/dev/null; then
+	echo "Run 'dprint --help' to get started"
+else
+	echo "Manually add the directory to your \$HOME/.bash_profile (or similar)"
+	echo "  export DPRINT_INSTALL=\"$dprint_install\""
+	echo "  export PATH=\"\$DPRINT_INSTALL/bin:\$PATH\""
+	echo "Run '$exe --help' to get started"
+fi
