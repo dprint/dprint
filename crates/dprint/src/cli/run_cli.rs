34fc365d9e87f9d5c193ea9527bb1f168bd32cd4
David Sherret <dsherret@gmail.com>
2020-07-25 13:48:25 -0400

    Add tests for erroring while formatting with host.

--- crates/dprint/src/cli/run_cli.rs
+++ crates/dprint/src/cli/run_cli.rs
@@ -984,6 +984,16 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn it_should_error_calling_process_plugin_with_wasm_plugin_and_process_plugin_errors() {
+        let environment = get_initialized_test_environment_with_remote_wasm_and_process_plugin().await.unwrap();
+        let file_path = PathBuf::from("/file.txt");
+        environment.write_file(&file_path, "plugin: should_error").unwrap();
+        let error_message = run_test_cli(vec!["fmt", "/file.txt"], &environment).await.err().unwrap();
+        assert_eq!(error_message.to_string(), "Had 1 error(s) formatting.");
+        assert_eq!(environment.get_logged_errors(), vec![String::from("Error formatting /file.txt. Message: Did error.")]);
+    }
+
+    #[tokio::test]
     async fn it_should_format_calling_other_plugin_with_process_plugin_and_no_plugin_exists() {
         let environment = get_initialized_test_environment_with_remote_process_plugin().await.unwrap();
         let file_path = PathBuf::from("/file.txt_ps");
@@ -1003,6 +1013,16 @@ mod tests {
         assert_eq!(environment.get_logged_messages(), vec![get_singular_formatted_text()]);
         assert_eq!(environment.get_logged_errors().len(), 0);
         assert_eq!(environment.read_file(&file_path).unwrap(), "format this text_formatted");
+    }
+
+    #[tokio::test]
+    async fn it_should_error_calling_wasm_plugin_with_process_plugin_and_wasm_plugin_errors() {
+        let environment = get_initialized_test_environment_with_remote_wasm_and_process_plugin().await.unwrap();
+        let file_path = PathBuf::from("/file.txt_ps");
+        environment.write_file(&file_path, "plugin: should_error").unwrap();
+        let error_message = run_test_cli(vec!["fmt", "/file.txt_ps"], &environment).await.err().unwrap();
+        assert_eq!(error_message.to_string(), "Had 1 error(s) formatting.");
+        assert_eq!(environment.get_logged_errors(), vec![String::from("Error formatting /file.txt_ps. Message: Did error.")]);
     }
 
     #[tokio::test]
