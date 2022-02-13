6ebb49ad31df41926ab09c30fa5ae5912a50e17e
Josh Leeb-du Toit <mail@joshleeb.com>
2022-02-14 03:16:47 +1100

    core: replace CounterCell with IdCounter (#460)

--- crates/core/src/formatting/utils/mod.rs
+++ crates/core/src/formatting/utils/mod.rs
@@ -1,6 +1,5 @@
-mod counter_cell;
-pub mod string_utils;
 mod thread_local_bump_allocator;
 
-pub(crate) use counter_cell::*;
+pub mod string_utils;
+
 pub(crate) use thread_local_bump_allocator::*;
