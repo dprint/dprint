1682b7896b1d99c5a827a901afb7bb91de06e81e
dsherret <dsherret@users.noreply.github.com>
2022-05-01 21:43:02 +0000

    Deploying to gh-pages from  @ ae22ee5949a94ca3a0218b8cc4dddf688ddee648 🚀

--- scripts.js
+++ scripts.js
@@ -1,18 +1,251 @@
-var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.arrayIteratorImpl=function(a){var d=0;return function(){return d<a.length?{done:!1,value:a[d++]}:{done:!0}}};$jscomp.arrayIterator=function(a){return{next:$jscomp.arrayIteratorImpl(a)}};$jscomp.makeIterator=function(a){var d="undefined"!=typeof Symbol&&Symbol.iterator&&a[Symbol.iterator];return d?d.call(a):$jscomp.arrayIterator(a)};$jscomp.ASSUME_ES5=!1;$jscomp.ASSUME_NO_NATIVE_MAP=!1;$jscomp.ASSUME_NO_NATIVE_SET=!1;
-$jscomp.defineProperty=$jscomp.ASSUME_ES5||"function"==typeof Object.defineProperties?Object.defineProperty:function(a,d,e){a!=Array.prototype&&a!=Object.prototype&&(a[d]=e.value)};$jscomp.getGlobal=function(a){return"undefined"!=typeof window&&window===a?a:"undefined"!=typeof global&&null!=global?global:a};$jscomp.global=$jscomp.getGlobal(this);$jscomp.SYMBOL_PREFIX="jscomp_symbol_";$jscomp.initSymbol=function(){$jscomp.initSymbol=function(){};$jscomp.global.Symbol||($jscomp.global.Symbol=$jscomp.Symbol)};
-$jscomp.Symbol=function(){var a=0;return function(d){return $jscomp.SYMBOL_PREFIX+(d||"")+a++}}();$jscomp.initSymbolIterator=function(){$jscomp.initSymbol();var a=$jscomp.global.Symbol.iterator;a||(a=$jscomp.global.Symbol.iterator=$jscomp.global.Symbol("iterator"));"function"!=typeof Array.prototype[a]&&$jscomp.defineProperty(Array.prototype,a,{configurable:!0,writable:!0,value:function(){return $jscomp.iteratorPrototype($jscomp.arrayIteratorImpl(this))}});$jscomp.initSymbolIterator=function(){}};
-$jscomp.initSymbolAsyncIterator=function(){$jscomp.initSymbol();var a=$jscomp.global.Symbol.asyncIterator;a||(a=$jscomp.global.Symbol.asyncIterator=$jscomp.global.Symbol("asyncIterator"));$jscomp.initSymbolAsyncIterator=function(){}};$jscomp.iteratorPrototype=function(a){$jscomp.initSymbolIterator();a={next:a};a[$jscomp.global.Symbol.iterator]=function(){return this};return a};
-$jscomp.iteratorFromArray=function(a,d){$jscomp.initSymbolIterator();a instanceof String&&(a+="");var e=0,c={next:function(){if(e<a.length){var b=e++;return{value:d(b,a[b]),done:!1}}c.next=function(){return{done:!0,value:void 0}};return c.next()}};c[Symbol.iterator]=function(){return c};return c};
-$jscomp.polyfill=function(a,d,e,c){if(d){e=$jscomp.global;a=a.split(".");for(c=0;c<a.length-1;c++){var b=a[c];b in e||(e[b]={});e=e[b]}a=a[a.length-1];c=e[a];d=d(c);d!=c&&null!=d&&$jscomp.defineProperty(e,a,{configurable:!0,writable:!0,value:d})}};$jscomp.polyfill("Array.prototype.keys",function(a){return a?a:function(){return $jscomp.iteratorFromArray(this,function(a){return a})}},"es6","es3");$jscomp.owns=function(a,d){return Object.prototype.hasOwnProperty.call(a,d)};
-$jscomp.assign="function"==typeof Object.assign?Object.assign:function(a,d){for(var e=1;e<arguments.length;e++){var c=arguments[e];if(c)for(var b in c)$jscomp.owns(c,b)&&(a[b]=c[b])}return a};$jscomp.polyfill("Object.assign",function(a){return a||$jscomp.assign},"es6","es3");$jscomp.findInternal=function(a,d,e){a instanceof String&&(a=String(a));for(var c=a.length,b=0;b<c;b++){var h=a[b];if(d.call(e,h,b,a))return{i:b,v:h}}return{i:-1,v:void 0}};
-$jscomp.polyfill("Array.prototype.find",function(a){return a?a:function(a,e){return $jscomp.findInternal(this,a,e).v}},"es6","es3");(function(a){function d(){a.replacePluginUrls();a.replaceConfigTable();a.addNavBurgerEvent()}"complete"===document.readyState||"interactive"===document.readyState?setTimeout(d,0):document.addEventListener("DOMContentLoaded",d)})(window.Dprint||(window.Dprint={}));
-(function(a){a.addNavBurgerEvent=function(){var a=document.getElementById("navbarBurger");a.addEventListener("click",function(){a.classList.toggle("is-active");document.getElementById(a.dataset.target).classList.toggle("is-active")})}})(window.Dprint||(window.Dprint={}));
-(function(a){function d(){for(var a=[],b=document.getElementsByClassName("plugin-config-table"),e=0;e<b.length;e++){var g=b.item(e);a.push({element:g,url:g.dataset.url})}return a}function e(a){return fetch(a).then(function(a){return a.json()}).then(function(a){function e(a){null==b[a]&&(b[a]={astSpecificProperties:[]})}for(var b={},d=0,c=$jscomp.makeIterator(Object.keys(a.properties)),f=c.next();!f.done;f=c.next())if(f=f.value,"$schema"!==f&&"deno"!==f&&"locked"!==f){var k=a.properties[f];if(k.$ref){var m=
-a.definitions[f];null!=m?(e(f),b[f]=Object.assign(b[f],m),b[f].order=d++,b[f].name=f):(k=k.$ref.replace("#/definitions/",""),e(k),b[k].astSpecificProperties.push(f))}else e(f),b[f]=Object.assign(b[f],k),b[f].order=d++,b[f].name=f}a=[];d=Object.keys(b);for(c=0;c<d.length;c++)a.push(b[d[c]]);a.sort(function(a,b){return a.order-b.order});return a})}a.replaceConfigTable=function(){var a=d();0<a.length&&a.forEach(function(a){e(a.url).then(function(b){function e(a){return"string"===typeof a?'"'+a+'"':a.toString()}
-var d=a.element;d.innerHTML='<p>This information was auto generated from <a href="'+a.url+'">'+a.url+"</a>.</p>";b.forEach(function(a){var b=document.createElement("div");d.appendChild(b);var c=document.createElement("h2");"preferSingleLine"===a.name&&(a.name+=" (Very Experimental)");c.textContent=a.name;b.appendChild(c);c=document.createElement("p");c.textContent=a.description;b.appendChild(c);var h=document.createElement("ul");b.appendChild(h);if(a.oneOf)a.oneOf.forEach(function(b){var c=document.createElement("li");
-h.appendChild(c);var d=document.createElement("strong");d.textContent=e(b.const);c.appendChild(d);null!=b.description&&0<b.description.length&&c.append(" - "+b.description);b.const===a.default&&c.append(" (Default)")});else{c=document.createElement("li");h.appendChild(c);var g=document.createElement("strong");g.textContent="Type: ";c.appendChild(g);c.append(a.type);c=document.createElement("li");h.appendChild(c);g=document.createElement("strong");g.textContent="Default: ";c.appendChild(g);c.append(e(a.default))}if(null!=
-a.astSpecificProperties&&0<a.astSpecificProperties.length){c=document.createElement("p");c.textContent="AST node specific configuration property names:";b.appendChild(c);var l=document.createElement("ul");b.appendChild(l);a.astSpecificProperties.forEach(function(a){var b=document.createElement("li");b.textContent=e(a);l.appendChild(b)})}})})})}})(window.Dprint||(window.Dprint={}));
-(function(a){function d(){for(var a=document.getElementsByClassName("hljs-string"),c=[],d=0;d<a.length;d++){var l=a.item(d);switch(l.textContent){case e("https://plugins.dprint.dev/typescript-x.x.x.wasm"):case e("https://plugins.dprint.dev/json-x.x.x.wasm"):case e("https://plugins.dprint.dev/markdown-x.x.x.wasm"):case e("https://plugins.dprint.dev/toml-x.x.x.wasm"):case e("https://plugins.dprint.dev/dockerfile-x.x.x.wasm"):c.push(l)}}return c}function e(a){return'"'+a+'"'}function c(){function a(a,
-b){a=a.latest.find(function(a){return a.name===b});if(null==a)throw Error("Could not find plugin with name "+b);return a.url}return fetch("https://plugins.dprint.dev/info.json").then(function(a){return a.json()}).then(function(b){if(4!==b.schemaVersion)throw Error("Expected schema version 4, but found "+b.schemaVersion);return{typescript:a(b,"dprint-plugin-typescript"),json:a(b,"dprint-plugin-json"),markdown:a(b,"dprint-plugin-markdown"),toml:a(b,"dprint-plugin-toml"),dockerfile:a(b,"dprint-plugin-dockerfile")}})}
-a.replacePluginUrls=function(){var a=d();0<a.length&&c().then(function(b){for(var c=0;c<a.length;c++){var d=a[c];switch(d.textContent){case e("https://plugins.dprint.dev/typescript-x.x.x.wasm"):d.textContent=e(b.typescript);break;case e("https://plugins.dprint.dev/json-x.x.x.wasm"):d.textContent=e(b.json);break;case e("https://plugins.dprint.dev/markdown-x.x.x.wasm"):d.textContent=e(b.markdown);break;case e("https://plugins.dprint.dev/toml-x.x.x.wasm"):d.textContent=e(b.toml);break;case e("https://plugins.dprint.dev/dockerfile-x.x.x.wasm"):d.textContent=
-e(b.dockerfile)}}})}})(window.Dprint||(window.Dprint={}));
+(() => {
+  var __defProp = Object.defineProperty;
+  var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+
+  // src/scripts/nav-burger.js
+  function addNavBurgerEvent() {
+    const navBurger = document.getElementById("navbarBurger");
+    navBurger.addEventListener("click", () => {
+      navBurger.classList.toggle("is-active");
+      document.getElementById(navBurger.dataset.target).classList.toggle("is-active");
+    });
+  }
+  __name(addNavBurgerEvent, "addNavBurgerEvent");
+
+  // src/scripts/plugin-config-table-replacer.js
+  function replaceConfigTable() {
+    const items = getPluginConfigTableItems();
+    if (items.length > 0) {
+      items.forEach(function(item) {
+        getDprintPluginConfig(item.url).then((properties) => {
+          const element = item.element;
+          element.innerHTML = '<p>This information was auto generated from <a href="' + item.url + '">' + item.url + "</a>.</p>";
+          properties.forEach(function(property) {
+            const propertyContainer = document.createElement("div");
+            element.appendChild(propertyContainer);
+            const propertyTitle = document.createElement("h2");
+            if (property.name === "preferSingleLine") {
+              property.name += " (Very Experimental)";
+            }
+            propertyTitle.textContent = property.name;
+            propertyContainer.appendChild(propertyTitle);
+            const propertyDesc = document.createElement("p");
+            propertyDesc.textContent = property.description;
+            propertyContainer.appendChild(propertyDesc);
+            const infoContainer = document.createElement("ul");
+            propertyContainer.appendChild(infoContainer);
+            if (property.oneOf) {
+              property.oneOf.forEach(function(oneOf) {
+                const oneOfContainer = document.createElement("li");
+                infoContainer.appendChild(oneOfContainer);
+                const prefix = document.createElement("strong");
+                prefix.textContent = valueToText(oneOf.const);
+                oneOfContainer.appendChild(prefix);
+                if (oneOf.description != null && oneOf.description.length > 0) {
+                  oneOfContainer.append(" - " + oneOf.description);
+                }
+                if (oneOf.const === property.default) {
+                  oneOfContainer.append(" (Default)");
+                }
+              });
+            } else {
+              const typeContainer = document.createElement("li");
+              infoContainer.appendChild(typeContainer);
+              const typePrefix = document.createElement("strong");
+              typePrefix.textContent = "Type: ";
+              typeContainer.appendChild(typePrefix);
+              typeContainer.append(property.type);
+              const defaultContainer = document.createElement("li");
+              infoContainer.appendChild(defaultContainer);
+              const defaultPrefix = document.createElement("strong");
+              defaultPrefix.textContent = "Default: ";
+              defaultContainer.appendChild(defaultPrefix);
+              defaultContainer.append(valueToText(property.default));
+            }
+            if (property.astSpecificProperties != null && property.astSpecificProperties.length > 0) {
+              const astSpecificPropertiesPrefix = document.createElement("p");
+              astSpecificPropertiesPrefix.textContent = "AST node specific configuration property names:";
+              propertyContainer.appendChild(astSpecificPropertiesPrefix);
+              const astSpecificPropertyNamesContainer = document.createElement("ul");
+              propertyContainer.appendChild(astSpecificPropertyNamesContainer);
+              property.astSpecificProperties.forEach(function(propName) {
+                const propertyNameLi = document.createElement("li");
+                propertyNameLi.textContent = valueToText(propName);
+                astSpecificPropertyNamesContainer.appendChild(propertyNameLi);
+              });
+            }
+          });
+          function valueToText(value) {
+            if (typeof value === "string") {
+              return '"' + value + '"';
+            }
+            return value.toString();
+          }
+          __name(valueToText, "valueToText");
+        });
+      });
+    }
+  }
+  __name(replaceConfigTable, "replaceConfigTable");
+  function getPluginConfigTableItems() {
+    const result = [];
+    const elements = document.getElementsByClassName("plugin-config-table");
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements.item(i);
+      result.push({
+        element,
+        url: element.dataset.url
+      });
+    }
+    return result;
+  }
+  __name(getPluginConfigTableItems, "getPluginConfigTableItems");
+  function getDprintPluginConfig(configSchemaUrl) {
+    return fetch(configSchemaUrl).then((response) => {
+      return response.json();
+    }).then((json) => {
+      const properties = {};
+      let order = 0;
+      for (const propertyName of Object.keys(json.properties)) {
+        if (propertyName === "$schema" || propertyName === "deno" || propertyName === "locked") {
+          continue;
+        }
+        const property = json.properties[propertyName];
+        if (property["$ref"]) {
+          const definition = json.definitions[propertyName];
+          if (definition != null) {
+            ensurePropertyName(propertyName);
+            properties[propertyName] = Object.assign(properties[propertyName], definition);
+            properties[propertyName].order = order++;
+            properties[propertyName].name = propertyName;
+          } else {
+            const derivedPropName = property["$ref"].replace("#/definitions/", "");
+            ensurePropertyName(derivedPropName);
+            properties[derivedPropName].astSpecificProperties.push(propertyName);
+          }
+        } else {
+          ensurePropertyName(propertyName);
+          properties[propertyName] = Object.assign(properties[propertyName], property);
+          properties[propertyName].order = order++;
+          properties[propertyName].name = propertyName;
+        }
+      }
+      const propertyArray = [];
+      const propertyKeys = Object.keys(properties);
+      for (let i = 0; i < propertyKeys.length; i++) {
+        const propName = propertyKeys[i];
+        propertyArray.push(properties[propName]);
+      }
+      propertyArray.sort((a, b) => a.order - b.order);
+      return propertyArray;
+      function ensurePropertyName(propertyName) {
+        if (properties[propertyName] == null) {
+          properties[propertyName] = {
+            astSpecificProperties: []
+          };
+        }
+      }
+      __name(ensurePropertyName, "ensurePropertyName");
+    });
+  }
+  __name(getDprintPluginConfig, "getDprintPluginConfig");
+
+  // src/scripts/plugin-url-replacer.js
+  var typescriptUrl = "https://plugins.dprint.dev/typescript-x.x.x.wasm";
+  var jsonUrl = "https://plugins.dprint.dev/json-x.x.x.wasm";
+  var markdownUrl = "https://plugins.dprint.dev/markdown-x.x.x.wasm";
+  var tomlUrl = "https://plugins.dprint.dev/toml-x.x.x.wasm";
+  var dockerfileUrl = "https://plugins.dprint.dev/dockerfile-x.x.x.wasm";
+  var pluginInfoUrl = "https://plugins.dprint.dev/info.json";
+  var schemaVersion = 4;
+  function replacePluginUrls() {
+    const elements = getPluginUrlElements();
+    if (elements.length > 0) {
+      getPluginInfo().then((urls) => {
+        for (let i = 0; i < elements.length; i++) {
+          const element = elements[i];
+          switch (element.textContent) {
+            case getWithQuotes(typescriptUrl):
+              element.textContent = getWithQuotes(urls["typescript"]);
+              break;
+            case getWithQuotes(jsonUrl):
+              element.textContent = getWithQuotes(urls["json"]);
+              break;
+            case getWithQuotes(markdownUrl):
+              element.textContent = getWithQuotes(urls["markdown"]);
+              break;
+            case getWithQuotes(tomlUrl):
+              element.textContent = getWithQuotes(urls["toml"]);
+              break;
+            case getWithQuotes(dockerfileUrl):
+              element.textContent = getWithQuotes(urls["dockerfile"]);
+              break;
+          }
+        }
+      });
+    }
+  }
+  __name(replacePluginUrls, "replacePluginUrls");
+  function getPluginUrlElements() {
+    const stringElements = document.getElementsByClassName("hljs-string");
+    const result = [];
+    for (let i = 0; i < stringElements.length; i++) {
+      const stringElement = stringElements.item(i);
+      switch (stringElement.textContent) {
+        case getWithQuotes(typescriptUrl):
+        case getWithQuotes(jsonUrl):
+        case getWithQuotes(markdownUrl):
+        case getWithQuotes(tomlUrl):
+        case getWithQuotes(dockerfileUrl):
+          result.push(stringElement);
+          break;
+      }
+    }
+    return result;
+  }
+  __name(getPluginUrlElements, "getPluginUrlElements");
+  function getWithQuotes(text) {
+    return '"' + text + '"';
+  }
+  __name(getWithQuotes, "getWithQuotes");
+  function getPluginInfo() {
+    return fetch(pluginInfoUrl).then((response) => {
+      return response.json();
+    }).then((data) => {
+      if (data.schemaVersion !== schemaVersion) {
+        throw new Error("Expected schema version " + schemaVersion + ", but found " + data.schemaVersion);
+      }
+      return {
+        typescript: getUrlForPlugin(data, "dprint-plugin-typescript"),
+        json: getUrlForPlugin(data, "dprint-plugin-json"),
+        markdown: getUrlForPlugin(data, "dprint-plugin-markdown"),
+        toml: getUrlForPlugin(data, "dprint-plugin-toml"),
+        dockerfile: getUrlForPlugin(data, "dprint-plugin-dockerfile")
+      };
+    });
+    function getUrlForPlugin(data, pluginName) {
+      const pluginInfo = data.latest.find((pluginInfo2) => {
+        return pluginInfo2.name === pluginName;
+      });
+      if (pluginInfo == null) {
+        throw new Error("Could not find plugin with name " + pluginName);
+      }
+      return pluginInfo.url;
+    }
+    __name(getUrlForPlugin, "getUrlForPlugin");
+  }
+  __name(getPluginInfo, "getPluginInfo");
+
+  // src/scripts.js
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+    setTimeout(onLoad, 0);
+  } else {
+    document.addEventListener("DOMContentLoaded", onLoad);
+  }
+  function onLoad() {
+    replacePluginUrls();
+    replaceConfigTable();
+    addNavBurgerEvent();
+  }
+  __name(onLoad, "onLoad");
+})();
