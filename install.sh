7bd8d709d9813a98d3d53e3063f0b8613d9a4a50
dsherret <dsherret@users.noreply.github.com>
2023-01-17 12:22:37 +0000

    Deploying to gh-pages from  @ 2f9a9d2c39739bb73fdbf0a50a8a379421bcacc4 🚀

--- install.sh
+++ install.sh
@@ -16,7 +16,13 @@ else
 	"Darwin x86_64") target="x86_64-apple-darwin" ;;
 	"Darwin arm64") target="aarch64-apple-darwin" ;;
 	"Linux aarch64") target="aarch64-unknown-linux-gnu" ;;
-	*) target="x86_64-unknown-linux-gnu" ;;
+	*)
+		is_musl=$(ldd /bin/sh | grep 'musl' || true)
+		if [ -z "$is_musl" ]; then
+			target="x86_64-unknown-linux-gnu"
+		else
+			target="x86_64-unknown-linux-musl"
+		fi ;;
 	esac
 fi
 
