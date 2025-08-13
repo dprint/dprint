93934db9453f0529a6deeeeb02ae8c44e85ec6dc
Zhiyuan Hong <28915578+hzy@users.noreply.github.com>
2025-08-14 03:09:47 +0800

    fix: ignore empty proxy env (#1014)

--- crates/dprint/src/utils/url.rs
+++ crates/dprint/src/utils/url.rs
@@ -40,6 +40,7 @@ impl ProxyProvider for RealProxyUrlProvider {
       std::env::var(env_var_name.to_uppercase())
         .ok()
         .or_else(|| std::env::var(env_var_name.to_lowercase()).ok())
+        .and_then(|v| if v.is_empty() { None } else { Some(v) })
     }
 
     static HTTP_PROXY: OnceLock<Option<String>> = OnceLock::new();
