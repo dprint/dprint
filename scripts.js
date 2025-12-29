38abcfefd70e2d3a1c1290f45b95dddbdb466f59
dsherret <dsherret@users.noreply.github.com>
2025-12-29 19:17:06 +0000

    Deploying to gh-pages from  @ b3ce8b98c0ffbab46e46dcb6f5315a60ddce216e 🚀

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
   var __defProp = Object.defineProperty;
   var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-  // deno:file:///home/runner/work/dprint/dprint/website/src/scripts/nav-burger.js
+  // scripts/nav-burger.js
   function addNavBurgerEvent() {
     const navBurger = document.getElementById("navbarBurger");
     navBurger.addEventListener("click", () => {
@@ -12,7 +12,7 @@
   }
   __name(addNavBurgerEvent, "addNavBurgerEvent");
 
-  // deno:file:///home/runner/work/dprint/dprint/website/src/scripts/plugin-config-table-replacer.js
+  // scripts/plugin-config-table-replacer.js
   function replaceConfigTable() {
     const items = getPluginConfigTableItems();
     if (items.length > 0) {
@@ -195,68 +195,33 @@
   }
   __name(getDprintPluginConfig, "getDprintPluginConfig");
 
-  // deno:file:///home/runner/work/dprint/dprint/website/src/scripts/plugin-url-replacer.js
-  var typescriptUrl = "https://plugins.dprint.dev/typescript-x.x.x.wasm";
-  var jsonUrl = "https://plugins.dprint.dev/json-x.x.x.wasm";
-  var markdownUrl = "https://plugins.dprint.dev/markdown-x.x.x.wasm";
-  var tomlUrl = "https://plugins.dprint.dev/toml-x.x.x.wasm";
-  var dockerfileUrl = "https://plugins.dprint.dev/dockerfile-x.x.x.wasm";
-  var biomeUrl = "https://plugins.dprint.dev/biome-x.x.x.wasm";
-  var oxcUrl = "https://plugins.dprint.dev/oxc-x.x.x.wasm";
-  var ruffUrl = "https://plugins.dprint.dev/ruff-x.x.x.wasm";
-  var jupyterUrl = "https://plugins.dprint.dev/jupyter-x.x.x.wasm";
-  var malvaUrl = "https://plugins.dprint.dev/g-plane/malva-vx.x.x.wasm";
-  var markupFmtUrl = "https://plugins.dprint.dev/g-plane/markup_fmt-vx.x.x.wasm";
-  var yamlUrl = "https://plugins.dprint.dev/g-plane/pretty_yaml-vx.x.x.wasm";
-  var graphqlUrl = "https://plugins.dprint.dev/g-plane/pretty_graphql-vx.x.x.wasm";
+  // scripts/plugin-url-replacer.js
   var pluginInfoUrl = "https://plugins.dprint.dev/info.json";
   var schemaVersion = 4;
+  var pluginPlaceholders = /* @__PURE__ */ new Map([
+    ['"https://plugins.dprint.dev/typescript-x.x.x.wasm"', "dprint-plugin-typescript"],
+    ['"https://plugins.dprint.dev/json-x.x.x.wasm"', "dprint-plugin-json"],
+    ['"https://plugins.dprint.dev/markdown-x.x.x.wasm"', "dprint-plugin-markdown"],
+    ['"https://plugins.dprint.dev/toml-x.x.x.wasm"', "dprint-plugin-toml"],
+    ['"https://plugins.dprint.dev/dockerfile-x.x.x.wasm"', "dprint-plugin-dockerfile"],
+    ['"https://plugins.dprint.dev/biome-x.x.x.wasm"', "dprint-plugin-biome"],
+    ['"https://plugins.dprint.dev/oxc-x.x.x.wasm"', "dprint-plugin-oxc"],
+    ['"https://plugins.dprint.dev/ruff-x.x.x.wasm"', "dprint-plugin-ruff"],
+    ['"https://plugins.dprint.dev/jupyter-x.x.x.wasm"', "dprint-plugin-jupyter"],
+    ['"https://plugins.dprint.dev/g-plane/malva-vx.x.x.wasm"', "g-plane/malva"],
+    ['"https://plugins.dprint.dev/g-plane/markup_fmt-vx.x.x.wasm"', "g-plane/markup_fmt"],
+    ['"https://plugins.dprint.dev/g-plane/pretty_yaml-vx.x.x.wasm"', "g-plane/pretty_yaml"],
+    ['"https://plugins.dprint.dev/g-plane/pretty_graphql-vx.x.x.wasm"', "g-plane/pretty_graphql"]
+  ]);
   function replacePluginUrls() {
     const elements = getPluginUrlElements();
     if (elements.length > 0) {
-      getPluginInfo().then((urls) => {
-        for (let i = 0; i < elements.length; i++) {
-          const element = elements[i];
-          switch (element.textContent) {
-            case getWithQuotes(typescriptUrl):
-              element.textContent = getWithQuotes(urls["typescript"]);
-              break;
-            case getWithQuotes(jsonUrl):
-              element.textContent = getWithQuotes(urls["json"]);
-              break;
-            case getWithQuotes(markdownUrl):
-              element.textContent = getWithQuotes(urls["markdown"]);
-              break;
-            case getWithQuotes(tomlUrl):
-              element.textContent = getWithQuotes(urls["toml"]);
-              break;
-            case getWithQuotes(dockerfileUrl):
-              element.textContent = getWithQuotes(urls["dockerfile"]);
-              break;
-            case getWithQuotes(biomeUrl):
-              element.textContent = getWithQuotes(urls["biome"]);
-              break;
-            case getWithQuotes(oxcUrl):
-              element.textContent = getWithQuotes(urls["oxc"]);
-              break;
-            case getWithQuotes(ruffUrl):
-              element.textContent = getWithQuotes(urls["ruff"]);
-              break;
-            case getWithQuotes(jupyterUrl):
-              element.textContent = getWithQuotes(urls["jupyter"]);
-              break;
-            case getWithQuotes(malvaUrl):
-              element.textContent = getWithQuotes(urls["malva"]);
-              break;
-            case getWithQuotes(markupFmtUrl):
-              element.textContent = getWithQuotes(urls["markup_fmt"]);
-              break;
-            case getWithQuotes(yamlUrl):
-              element.textContent = getWithQuotes(urls["yaml"]);
-              break;
-            case getWithQuotes(graphqlUrl):
-              element.textContent = getWithQuotes(urls["graphql"]);
-              break;
+      getPluginInfo().then((pluginUrls) => {
+        for (const element of getPluginUrlElements()) {
+          const pluginName = pluginPlaceholders.get(element.textContent);
+          const url = pluginUrls.get(pluginName);
+          if (url != null) {
+            element.textContent = '"' + url + '"';
           }
         }
       });
@@ -268,67 +233,28 @@
     const result = [];
     for (let i = 0; i < stringElements.length; i++) {
       const stringElement = stringElements.item(i);
-      switch (stringElement.textContent) {
-        case getWithQuotes(typescriptUrl):
-        case getWithQuotes(jsonUrl):
-        case getWithQuotes(markdownUrl):
-        case getWithQuotes(tomlUrl):
-        case getWithQuotes(dockerfileUrl):
-        case getWithQuotes(biomeUrl):
-        case getWithQuotes(ruffUrl):
-        case getWithQuotes(jupyterUrl):
-        case getWithQuotes(malvaUrl):
-        case getWithQuotes(markupFmtUrl):
-        case getWithQuotes(yamlUrl):
-        case getWithQuotes(graphqlUrl):
-          result.push(stringElement);
-          break;
+      if (pluginPlaceholders.has(stringElement.textContent)) {
+        result.push(stringElement);
       }
     }
     return result;
   }
   __name(getPluginUrlElements, "getPluginUrlElements");
-  function getWithQuotes(text) {
-    return '"' + text + '"';
-  }
-  __name(getWithQuotes, "getWithQuotes");
   function getPluginInfo() {
-    return fetch(pluginInfoUrl).then((response) => {
-      return response.json();
-    }).then((data) => {
+    return fetch(pluginInfoUrl).then((response) => response.json()).then((data) => {
       if (data.schemaVersion !== schemaVersion) {
         throw new Error("Expected schema version " + schemaVersion + ", but found " + data.schemaVersion);
       }
-      return {
-        typescript: getUrlForPlugin(data, "dprint-plugin-typescript"),
-        json: getUrlForPlugin(data, "dprint-plugin-json"),
-        markdown: getUrlForPlugin(data, "dprint-plugin-markdown"),
-        toml: getUrlForPlugin(data, "dprint-plugin-toml"),
-        dockerfile: getUrlForPlugin(data, "dprint-plugin-dockerfile"),
-        biome: getUrlForPlugin(data, "dprint-plugin-biome"),
-        oxc: getUrlForPlugin(data, "dprint-plugin-oxc"),
-        ruff: getUrlForPlugin(data, "dprint-plugin-ruff"),
-        jupyter: getUrlForPlugin(data, "dprint-plugin-jupyter"),
-        malva: getUrlForPlugin(data, "g-plane/malva"),
-        markup_fmt: getUrlForPlugin(data, "g-plane/markup_fmt"),
-        yaml: getUrlForPlugin(data, "g-plane/pretty_yaml"),
-        graphql: getUrlForPlugin(data, "g-plane/pretty_graphql")
-      };
-    });
-    function getUrlForPlugin(data, pluginName) {
-      const pluginInfo = data.latest.find((pluginInfo2) => {
-        return pluginInfo2.name === pluginName;
-      });
-      if (pluginInfo == null) {
-        throw new Error("Could not find plugin with name " + pluginName);
+      const result = /* @__PURE__ */ new Map();
+      for (const pluginInfo of data.latest) {
+        result.set(pluginInfo.name, pluginInfo.url);
       }
-      return pluginInfo.url;
-    }
-    __name(getUrlForPlugin, "getUrlForPlugin");
+      return result;
+    });
   }
   __name(getPluginInfo, "getPluginInfo");
 
-  // deno:file:///home/runner/work/dprint/dprint/website/src/scripts.js
+  // scripts.js
   if (document.readyState === "complete" || document.readyState === "interactive") {
     setTimeout(onLoad, 0);
   } else {
