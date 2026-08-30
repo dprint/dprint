4a01afb1aac068760ed9b8749646bf236c10c79d
David Sherret <dsherret@users.noreply.github.com>
2026-08-30 12:41:31 -0500

    Apply suggestion from @dsherret

--- .github/workflows/website.ts
+++ .github/workflows/website.ts
@@ -5,8 +5,8 @@ const buildSteps = step(
   { name: "Checkout", uses: "actions/checkout@v6", with: { "persist-credentials": false } },
   { uses: "denoland/setup-deno@v2", with: { cache: true, "deno-version": "canary" } },
   { name: "Install dependencies", run: "(cd website && deno install)" },
-  { name: "Test playground", run: "(cd website/playground && deno task test)" },
   { name: "Build playground", run: "(cd website/playground && deno task build)" },
+  { name: "Test playground", run: "(cd website/playground && deno task test)" },
   { name: "Build website", run: "(cd website && deno task build:ci)" },
 );
 
