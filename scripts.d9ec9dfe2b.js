1f79cff66140d34a0adc55e746f5f88f2d9b6859
dsherret <dsherret@users.noreply.github.com>
2026-09-10 14:33:13 +0000

    Deploying to gh-pages from  @ 95bb3b1a51ddbc3b0ddaf68f7e468822f519dcc1 🚀

--- scripts.d9ec9dfe2b.js
+++ scripts.d9ec9dfe2b.js
@@ -304,7 +304,10 @@
     ['"https://plugins.dprint.dev/g-plane/pretty_yaml-vx.x.x.wasm"', "g-plane/pretty_yaml"],
     ['"https://plugins.dprint.dev/g-plane/pretty_graphql-vx.x.x.wasm"', "g-plane/pretty_graphql"],
     ['"https://plugins.dprint.dev/jakebailey/gofumpt-vx.x.x.wasm"', "jakebailey/dprint-plugin-gofumpt"],
-    ['"https://plugins.dprint.dev/jolars/panache-x.x.x.wasm"', "jolars/panache"]
+    ['"https://plugins.dprint.dev/jolars/panache-x.x.x.wasm"', "jolars/panache"],
+    ['"https://plugins.dprint.dev/jolars/badness-vx.x.x.wasm"', "jolars/badness"],
+    ['"https://plugins.dprint.dev/jolars/arity-vx.x.x.wasm"', "jolars/arity"],
+    ['"https://plugins.dprint.dev/jolars/fatou-vx.x.x.wasm"', "jolars/fatou"]
   ]);
   function replacePluginUrls() {
     const elements = getPluginUrlElements();
